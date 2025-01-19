8740cfcde9cba9e77d569f9c94ce951f06fa15cd
clubmbusan <clubmbusan@naver.com>
2025-01-19 20:18:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -290,7 +290,7 @@ else if (propertyTypeSelect.value === 'commercial') {
 longTermDeductionAmount = profit * longTermDeductionRate;
 
  // ✅ 기존 코드 수정 (과세표준 정상 반영)
- taxableProfit = longTermDeductionAmount; // 공제 후 과세표준 반영
+ taxableProfit = profit - longTermDeductionAmount; // 공제 후 과세표준 반영
     
 // 기본공제 적용 (과세표준에서 차감) --공용
 const basicDeduction = propertyTypeSelect.value !== 'unregistered' ? 2500000 : 0; // 미등기 부동산 기본공제 없음
